3345c8bbf4ec0f614fec5c7f4f1f9aa98cc6649b
ilyaghrischenko <ilyahryshchenko@gmail.com>
2024-02-04 11:45:06 +0200

    added new class ReverseMusicPlayer

---
+++ ReverseMusicPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace MusicPlayer
+{
+    public class ReverseMusicPlayer(NoteSequence noteSequence)
+    {
+        private readonly NoteSequence _noteSequence = noteSequence;
+
+        public void Play()
+        {
+            CheckOS();
+
+            var reverseList = noteSequence._notes;
+            reverseList.Reverse();
+            foreach (var item in reverseList)
+            {
+                Beep(item._frequency, item._duration);
+            }
+        }
+        private void CheckOS()
+        {
+            PlatformID platform = Environment.OSVersion.Platform;
+            if (platform != PlatformID.Win32NT && platform != PlatformID.Win32Windows)
+            {
+                throw new Exception("PLay(): your OS does not support this action");
+            }
+        }
+    }
+}
--- obj/Debug/net8.0/MusicPlayer.AssemblyInfo.cs
+++ obj/Debug/net8.0/MusicPlayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicPlayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eedb956aff53bcaffa466ed96daae66f3f4d67bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicPlayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicPlayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

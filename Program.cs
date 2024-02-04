ae05aac42477daac77a5c975b751a033e1292cd4
ilyaghrischenko <ilyahryshchenko@gmail.com>
2024-02-04 21:14:09 +0200

    added try catch construction

--- Program.cs
+++ Program.cs
@@ -6,21 +6,28 @@ namespace MusicPlayer
     {
         static void Main(string[] args)
         {
-            var noteSequence = new NoteSequence();
+            try
+            {
+                var noteSequence = new NoteSequence();
 
-            noteSequence.AddNote(new Note<FirstOctave>(FirstOctave.F, NoteDuration.EightWithPoint));
-            noteSequence.AddNote(new Note<FirstOctave>(FirstOctave.F, NoteDuration.EightWithPoint));
-            noteSequence.AddNote(new Note<FirstOctave>(FirstOctave.F, NoteDuration.EightWithPoint));
-            noteSequence.AddNote(new Note<FirstOctave>(FirstOctave.F, NoteDuration.Sixth));
-            noteSequence.AddNote(new Note<FirstOctave>(FirstOctave.F, NoteDuration.SixthWithPoint));
-            noteSequence.AddNote(new Note<FirstOctave>(FirstOctave.F, NoteDuration.Eight));
-            noteSequence.AddNote(new Note<FirstOctave>(FirstOctave.F, NoteDuration.Quarter));
-            noteSequence.AddNote(new Note<FirstOctave>(FirstOctave.F, NoteDuration.EightWithPoint));
-            noteSequence.AddNote(new Note<FirstOctave>(FirstOctave.F, NoteDuration.EightWithPoint));
-            noteSequence.AddNote(new Note<FirstOctave>(FirstOctave.F, NoteDuration.EightWithPoint));
+                noteSequence.AddNote(new Note<FirstOctave>(FirstOctave.F, NoteDuration.EightWithPoint));
+                noteSequence.AddNote(new Note<FirstOctave>(FirstOctave.F, NoteDuration.EightWithPoint));
+                noteSequence.AddNote(new Note<FirstOctave>(FirstOctave.F, NoteDuration.EightWithPoint));
+                noteSequence.AddNote(new Note<FirstOctave>(FirstOctave.F, NoteDuration.Sixth));
+                noteSequence.AddNote(new Note<FirstOctave>(FirstOctave.F, NoteDuration.SixthWithPoint));
+                noteSequence.AddNote(new Note<FirstOctave>(FirstOctave.F, NoteDuration.Eight));
+                noteSequence.AddNote(new Note<FirstOctave>(FirstOctave.F, NoteDuration.Quarter));
+                noteSequence.AddNote(new Note<FirstOctave>(FirstOctave.F, NoteDuration.EightWithPoint));
+                noteSequence.AddNote(new Note<FirstOctave>(FirstOctave.F, NoteDuration.EightWithPoint));
+                noteSequence.AddNote(new Note<FirstOctave>(FirstOctave.F, NoteDuration.EightWithPoint));
 
-            var musicPlayer = new MusicPlayer(noteSequence);
-            musicPlayer.Play();
+                var musicPlayer = new MusicPlayer(noteSequence);
+                musicPlayer.Play();
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex.Message);
+            }
         }
     }
 }
